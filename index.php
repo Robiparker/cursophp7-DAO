20d63118dd41ca4ef197aa64ac3a37515d4b4ca7
Robinso Luis Gonçalves <robinho_parker@hotmail.com>
2019-08-07 18:43:24 -0300

    Fiz metodo de lista

--- index.php
+++ index.php
@@ -1,11 +1,47 @@
-<?php
+<?php 
 
 require_once("config.php");
 
-$Parker = new Usuario();
+//Carrega um usuário
+//$Parker = new Usuario();
+//$Parker->loadbyId(2);
+//echo $Parker;
 
-$Parker->loadbyId(2);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $Parker;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("pa");
+//echo json_encode($search);
 
-?>
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Parker", "Ro243618");
+
+echo $usuario;
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+/*
+//Alterar um usuário
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+*/
+
+//$usuario = new Usuario();
+
+//$usuario->loadById(7);
+
+//$usuario->delete();
+
+//echo $usuario;
+ ?>
